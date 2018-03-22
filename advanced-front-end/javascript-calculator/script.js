d6daa8671750e918b8dd6471a6ecad6f590615b8
Kagerjay <acpython777@gmail.com>
2018-03-21 20:17:11 -0400

    JS Start - Basic TODO MVC structure

--- advanced-front-end/javascript-calculator/script.js
+++ advanced-front-end/javascript-calculator/script.js
@@ -1,3 +1,20 @@
+// MODEL
+var operations = {
+  // It should have a way to addTwoNumbers
+  add : function(a,b){
+    return a+b;
+  },
+  subtract : function(a,b) {
+    return a-b;
+  },
+  multiply: function(a,b){
+    return a*b;
+  },
+  divide: function(a,b){
+    return a/b;
+  }
+}
+
+// VIEWS + CONTROLLER IN JQUERY
 $(document).ready(function(){
-    console.log("testing");
 });
